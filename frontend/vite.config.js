3df58dbd67bfdd9f5046dafbee26fed5bf55600a
Robert Wood <Rdwood44@gmail.com>
2026-05-02 14:20:55 -0400

    fix: allow all hosts in vite preview

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    allowedHosts: [
-      'https://fantasy-baseball-2.onrender.com'
-    ]
+    allowedHosts: 'all'
   }
 })
